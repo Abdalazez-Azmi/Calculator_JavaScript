079d9e51374a4e6b3af5aa132cdbd844860adf37
Abdalazez-Azmi <90723420+Abdalazez-Azmi@users.noreply.github.com>
2021-11-11 17:41:38 +0200

    Finsh

---
+++ index.js
@@ -0,0 +1,16 @@
+const calculator=(num1,mark,num2)=>{
+if (mark === '*') {
+    return num1 * num2
+}else if (mark === '/'){
+    return num1 + num2
+
+}else if (mark === '+'){
+    return num1 + num2
+
+}else if (mark === '-'){
+    return num1 - num2
+
+}
+
+
+}
